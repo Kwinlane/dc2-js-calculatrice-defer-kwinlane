de718e7dec4db5acde1aa578cabf86147be148fc
Kwinlane <102586514+Kwinlane@users.noreply.github.com>
2022-11-22 11:23:07 +0100

    Add files via upload
    'Fonctions de calculs'

---
+++ script.js
@@ -0,0 +1,25 @@
+var a;
+var b;
+var c;
+
+function sum(a, b){
+    c = a + b;
+}
+
+//let result = sum(1,2);
+//alert( result );
+
+function subs(a, b) {
+    c = a - b;
+}
+
+function mult(a, b){
+    c = a*b;
+}
+
+function div(a, b){
+    c = a/b;
+}
+
+mult (-5, 3)
+console.log(c)
